f08ddd0802882ea104e92020a3286a6ed94e3436
Juan Thomassie <jthomassie1@gmail.com>
2022-04-28 11:19:12 -0400

    update next.config for prod

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 module.exports = {
   reactStrictMode: true,
-  resolve: {
-    fallback: {
-      net: false,
-    },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
+      config.resolve.fallback.net = false;
+      config.resolve.fallback.dns = false;
+      config.resolve.fallback.tls = false;
+    }
+    return config;
   },
-  // eslint: {
-  //   ignoreDuringBuilds: true,
-  // },
 };
