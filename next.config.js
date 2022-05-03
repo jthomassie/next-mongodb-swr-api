2e0fe6f7093d8d146298942e9982ec73cd9b8edb
Juan Thomassie <jthomassie1@gmail.com>
2022-05-02 23:51:22 -0400

    update config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
 module.exports = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
+      config.resolve.fallback = { fs: false };
+      config.resolve.fallback = { dns: false };
+      config.resolve.fallback = { tls: false };
+      config.resolve.fallback = { net: false };
+    }
+    return config;
+  },
   reactStrictMode: true,
 };
